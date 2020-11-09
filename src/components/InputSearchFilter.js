4a35805bf04b3d281bf2d8c57f8d7bc3d53e321c
Gabypisoni <pisonigabriel@gmail.com>
2020-11-09 09:02:54 -0300

    comiteando cambio de componente showmove

--- src/components/InputSearchFilter.js
+++ src/components/InputSearchFilter.js
@@ -1,28 +1,22 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import Alert from '@material-ui/lab/Alert';
+import Alert from "@material-ui/lab/Alert";
 
 const InputSearchFilter = ({
-    charactersTrue,
+  charactersTrue,
   setCharactersTrue,
   setSearchTerm,
-
 }) => {
-  
-  const handleChange = event => {
-    
+  const handleChange = (event) => {
     setSearchTerm(event.target.value);
-    const regexValidation =/^[:A-Za-z0-9\s]+$/g;
+    const regexValidation = /^[:A-Za-z0-9\s]+$/g;
 
     const reg = regexValidation.test(event.target.value);
-
-    if (reg) {
+    if (reg || 0 === event.target.value.length) {
       return setCharactersTrue(true);
     } else {
       return setCharactersTrue(false);
     }
-
-
   };
 
   return (
@@ -35,27 +29,28 @@ const InputSearchFilter = ({
             id="search"
             onChange={(e) => handleChange(e)}
             variant="outlined"
-            style={{padding:25}}
+            style={{ padding: 25 }}
             placeholder="Busque una pelicula"
           />
         ) : (
           <div>
-          <TextField
-            error
-            id="outlined-error-helper-text"
-            helperText="Ingrese solo numeros o letras"
-            variant="outlined"
-            style={{padding:25}}
-            onChange={(e) => handleChange(e)}
-          />
+            <TextField
+              error
+              id="outlined-error-helper-text"
+              helperText="Ingrese solo numeros o letras"
+              variant="outlined"
+              style={{ padding: 25 }}
+              onChange={(e) => handleChange(e)}
+            />
             <Alert variant="filled" severity="error">
-       Error Ingresa Caracteres Validos y el nombre de la pelicula incluidos los : 
-          <p>* Numeros o  letras , espacios y 2 puntos</p>
-          <p>Vuelve a ingresar caracteres validos para que desaparezca volver a buscar el titulo de la pelicula que quieres comprar</p>
-          
-      </Alert>
+              Error Ingresa Caracteres Validos y el nombre de la pelicula
+              incluidos los :<p>* Numeros o letras , espacios y 2 puntos</p>
+              <p>
+                Vuelve a ingresar caracteres validos para que desaparezca volver
+                a buscar el titulo de la pelicula que quieres comprar
+              </p>
+            </Alert>
           </div>
-          
         )}
       </form>
     </div>
--- src/components/ShopMovie.js
+++ src/components/ShopMovie.js
@@ -7,33 +7,32 @@ import Search from "../components/InputSearchFilter";
 import "../styles/trolley.css";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import axios from 'axios';
-
+import axios from "axios";
 
 let theme = createMuiTheme();
 
 function ShowMovie() {
   const [movies, setMovies] = useState([]);
-  
+
   const [searchTerm, setSearchTerm] = React.useState("");
   const [searchResults, setSearchResults] = React.useState([]);
   const [trolley, setTrolley] = useState([]);
   const [charactersTrue, setCharactersTrue] = useState(true);
-  const [load, setLoad] = useState(false)
+  const [load, setLoad] = useState(false);
   let date = new Date().getFullYear();
 
   useEffect(() => {
     axios
-    .get('http://localhost:3002/')
-    .then(res => { 
-      setMovies(res.data)
-      console.log(res.data)
-      setSearchResults(res.data)      
-      setLoad(false)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .get("http://localhost:3002/")
+      .then((res) => {
+        setMovies(res.data);
+        console.log(res.data);
+        setSearchResults(res.data);
+        setLoad(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   useEffect(() => {
@@ -43,8 +42,8 @@ function ShowMovie() {
     setSearchResults(results);
   }, [searchTerm]);
 
-  if(load){
-    return <p>Cargando Peliculas....</p>
+  if (load) {
+    return <p>Cargando Peliculas....</p>;
   }
 
   return (
